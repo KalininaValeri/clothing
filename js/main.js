ba77ed2a02f0e34c443b59f0ed72ce1893bf662c
KalininaValeri <kalinina.valeri@gmail.com>
2016-04-27 23:29:44 +0600

    map

--- js/main.js
+++ js/main.js
@@ -2,4 +2,50 @@ $(function () {
     var body = $('body');
     // the height of the screen
     body.append('<style>header .height{height:' + $( window ).height() + 'px;}</style>');
+
+    initMap()
 })
+
+function initMap() {
+
+    var myLatLng = {lat: 54.981245, lng: 82.912314};
+
+    // Specify features and elements to define styles.
+    var styleArray = [
+        {
+            featureType: "all",
+            stylers: [
+                { saturation: -80 }
+            ]
+        },{
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [
+                { hue: "#00ffee" },
+                { saturation: 50 }
+            ]
+        },{
+            featureType: "poi.business",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+            ]
+        }
+    ];
+
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        scrollwheel: false,
+        // Apply the map style array to the map.
+        styles: styleArray,
+        zoom: 17
+    });
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: myLatLng,
+        title: 'Hello World!'
+    });
+}
+
