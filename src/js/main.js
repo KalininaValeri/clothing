90d795e622aa867362931a594e140c1e3a2227b7
KalininaValeri <kalinina.valeri@gmail.com>
2016-05-02 11:02:03 +0600

    width img

--- src/js/main.js
+++ src/js/main.js
@@ -3,49 +3,13 @@ $(function () {
     // the height of the screen
     body.append('<style>header .height{height:' + $( window ).height() + 'px;}</style>');
 
-    initMap()
+    YMaps();
 })
 
-function initMap() {
+YMaps.jQuery(function () {
+    // Создает экземпляр карты и привязывает его к созданному контейнеру
+    var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
 
-    var myLatLng = {lat: 54.981245, lng: 82.912314};
-
-    // Specify features and elements to define styles.
-    var styleArray = [
-        {
-            featureType: "all",
-            stylers: [
-                { saturation: -80 }
-            ]
-        },{
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [
-                { hue: "#00ffee" },
-                { saturation: 50 }
-            ]
-        },{
-            featureType: "poi.business",
-            elementType: "labels",
-            stylers: [
-                { visibility: "off" }
-            ]
-        }
-    ];
-
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        scrollwheel: false,
-        // Apply the map style array to the map.
-        styles: styleArray,
-        zoom: 17
-    });
-
-    var marker = new google.maps.Marker({
-        map: map,
-        position: myLatLng,
-        title: 'Hello World!'
-    });
-}
-
+    // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
+    map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 10);
+})
