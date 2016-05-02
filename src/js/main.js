b3f6d2875c5783eb70c91353a95fbe848399e836
KalininaValeri <kalinina.valeri@gmail.com>
2016-05-02 11:22:21 +0600

    add yandex maps

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,23 @@ $(function () {
     // the height of the screen
     body.append('<style>header .height{height:' + $( window ).height() + 'px;}</style>');
 
-    YMaps();
-})
+    //init();
+});
 
-YMaps.jQuery(function () {
-    // Создает экземпляр карты и привязывает его к созданному контейнеру
-    var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
+ymaps.ready(init);
+var myMap,
+    myPlacemark;
 
-    // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
-    map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 10);
-})
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+    });
+
+    myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+        hintContent: 'Москва!',
+        balloonContent: 'Столица России'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
